d497b5746d93b2e5f0a8c09ca213dae38d7e2131
jacklemay <clemay.lm@gmail.com>
2020-03-04 23:17:11 -0600

    ASas

--- src/routes/go/components/CTASingle.svelte
+++ src/routes/go/components/CTASingle.svelte
@@ -1,6 +1,11 @@
 <script>
   let submitted = false;
   let position = 0;
+  let visible = {
+      previous:false,
+      next:true
+
+  }
   let form = {
     name: "",
     project: "",
@@ -31,21 +36,59 @@
     if (position < 4) {
       position++;
     }
+    if(position == 4){
+        visible.next = false;
+    }else{
+        visible.next = true;
+    }
+    
+     if(position == 0){
+        visible.previous = false;
+    }else{
+        visible.previous = true;
+    }
   }
   function previousPosition() {
     if (position > 0) {
       position--;
     }
+
+   if(position == 4){
+        visible.next = false;
+    }else{
+        visible.next = true;
+    }
+    
+     if(position == 0){
+        visible.previous = false;
+    }else{
+        visible.previous = true;
+    }
   }
 
-  function alert(){
-      alert("event");
+  function focus0(){
+      position=0;
+  }
+  function focus1(){
+      position=1;
+  }
+  function focus2(){
+      position=2;
+  }
+  function focus3(){
+      position=3;
+  }
+  function focus4(){
+      position=4;
   }
 </script>
 
 <style>
   input {
     opacity:0;
+  }
+  .arrow{
+      opacity:0;
   }
   .visible {
     opacity:1;
@@ -62,33 +105,33 @@
     class:visible={position == 0}
     bind:value={form.name}
     
-    on:blur={nextPosition}
+    on:focus={focus0}
     placeholder="Name" />
   <input
     type="text"
     class:visible={position == 1}
     bind:value={form.project}
-    on:blur={nextPosition}
+    on:focus={focus1}
     placeholder="Type of Project" />
   <input
     type="text"
     class:visible={position == 2}
     bind:value={form.website}
-    on:blur={nextPosition}
+    on:focus={focus2}
     placeholder="Company" />
   <input
     type="text"
     class:visible={position == 3}
     bind:value={form.phone}
-    on:blur={nextPosition}
+    on:focus={focus3}
     placeholder="Phone" />
   <input
     type="text"
     class:visible={position == 4}
     bind:value={form.email}
-    on:blur={nextPosition}
+    on:focus={focus4}
     placeholder="Email" />
-  <div class="arrow arrow__left" on:click={nextPosition}>Next</div>
-  <div class="arrow arrow__right" on:click={previousPosition}>Previous</div>
+  <div class="arrow arrow__left"  class:visible={visible.next} on:click={nextPosition}>Next</div>
+  <div class="arrow arrow__right"  class:visible={visible.previous} on:click={previousPosition}>Previous</div>
   <button>Submit</button>
 </form>
