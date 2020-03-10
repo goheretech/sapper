70bc12cd3ee26bc7229f765477dd21b75e0f62eb
jacklemay <clemay.lm@gmail.com>
2020-03-10 00:37:42 -0500

    okk

---
+++ src/routes/insights/index.svelte
@@ -0,0 +1,27 @@
+<script>
+import { onMount } from "svelte";
+
+    let posts = [];
+
+    const apiURL = "https://gohere.ghost.io/ghost/api/v3/content/posts/?key=e8993b7d47d963da2ddb5361ce";
+    let data = [];
+    onMount(async function() {
+        const response = await fetch(apiURL);
+        data = await response.json();
+        posts = data.posts;
+        console.log('articles: ',posts);
+    });
+</script>
+
+<style>
+    .bg{
+        background:white ;
+    }
+</style>
+
+<div class="bg">
+{#each posts as post}
+    <h1>{post.title}</h1>
+    {@html post.html}
+{/each}
+</div>
